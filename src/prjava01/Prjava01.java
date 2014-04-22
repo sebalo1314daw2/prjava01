3b71b6eff316bfd81d1a5530bbb8c04c8b20d39c
sergio <sergio@sergio-laptop>
2014-04-22 20:48:07 +0200

    Versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -36,6 +36,8 @@ public class Prjava01 {
             bw.newLine();
             bw.write("<body>");
             bw.newLine();
+            bw.write("<h1>DAW2m08uf4pr2</h1>");
+            bw.newLine();
             bw.write("Novap&agrave;ginaweb");
             bw.newLine();
             bw.write("</body>");
